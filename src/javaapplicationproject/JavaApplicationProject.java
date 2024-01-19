400d670830f3862dd38da6607467fdf45960cc6c
HarnoorK29 <gill1014@sheridancollege.ca>
2024-01-19 13:07:25 -0500

    this is saved

---
+++ src/javaapplicationproject/JavaApplicationProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplicationproject;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplicationProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Harnoor Kaur Gill ....");
+        System.out.println("hey !! how are you ?")
+    }
+    
+}
